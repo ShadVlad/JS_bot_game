c3887fe896fe3ef7ec1bfe0b0e14f2428c055600
Vladimir <wowchik.shadrin@yandex.ru>
2021-04-08 16:28:16 +0300

    homework06 bot

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,48 @@
 let isNumber = function (n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
+
+let getHiddenNumber = function () {
+  return parseInt(Math.random() * 100 + 1);
+};
+let getUserNumber = function () {
+  let userAnswer = prompt("Угадай число от 1 до 100");
+  if (userAnswer == null) return userAnswer;
+  if (
+    !isNumber(userAnswer) ||
+    parseFloat(userAnswer) > 100 ||
+    parseFloat(userAnswer) < 1
+  ) {
+    confirm("Введи число от 1 до 100!");
+    getUserNumber();
+  }
+  return userAnswer;
+};
+
+function game() {
+  let hiddenNumber = getHiddenNumber();
+  //console.log("hiddenNumber: ", hiddenNumber);
+  let guess = function () {
+    let userNumber = getUserNumber();
+    //console.log("userNumber: ", userNumber);
+    if (userNumber === null) {
+      alert("Игра окончена");
+      return;
+    }
+
+    if (userNumber == hiddenNumber) {
+      alert("Поздравляю, Вы угадали!!!");
+      return;
+    }
+
+    if (userNumber > hiddenNumber) {
+      alert("Загаданное число меньше. Попробуй еще");
+    } else if (userNumber < hiddenNumber) {
+      alert("Загаданное число больше. Попробуй еще");
+    }
+    guess();
+  };
+  guess();
+}
+
+game();
